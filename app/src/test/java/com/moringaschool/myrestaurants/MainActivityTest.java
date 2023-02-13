9b0a5b4f6941ff8a1eda3099b3bc35aba16bb5fc
kajela <titoyut56@gmail.com>
2023-02-13 11:51:10 -0500

    add the intial files

---
+++ app/src/test/java/com/moringaschool/myrestaurants/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.moringaschool.myrestaurants;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
+
+public class MainActivityTest {
+
+}
