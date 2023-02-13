72c4d24e52336cfb445ffaa711967fda643f4e72
kajela <titoyut56@gmail.com>
2023-02-13 11:53:38 -0500

    add the initial files

--- app/src/test/java/com/moringaschool/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/myrestaurants/MainActivityTest.java
@@ -1,11 +1,23 @@
 package com.moringaschool.myrestaurants;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 
 @RunWith(RobolectricTestRunner.class)
 
 public class MainActivityTest {
+
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
 
 }
